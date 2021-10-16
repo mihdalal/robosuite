a5b510357876654cf9e9b5c91fbd1ddffebfeb2a
Murtaza Dalal <mdalal@andrew.cmu.edu>
2021-10-16 18:23:12 -0400

    added bin pick divider experiment

--- robosuite/models/arenas/bins_arena.py
+++ robosuite/models/arenas/bins_arena.py
@@ -3,6 +3,36 @@ from robosuite.models.arenas import Arena
 from robosuite.utils.mjcf_utils import xml_path_completion
 from robosuite.utils.mjcf_utils import array_to_string
 
+class BinArena(Arena):
+    """
+    Workspace that contains one divided bin.
+
+    Args:
+        bin1_pos (3-tuple): (x,y,z) position to place bin1
+        table_full_size (3-tuple): (L,W,H) full dimensions of the table
+        table_friction (3-tuple): (sliding, torsional, rolling) friction parameters of the table
+    """
+
+    def __init__(
+        self, bin1_pos=(0.1, -0.5, 0.8), table_offset = (0, 0, 0.8), table_full_size=(0.39, 0.49, 0.82), table_friction=(1, 0.005, 0.0001)
+    ):
+        super().__init__(xml_path_completion("arenas/bin_arena.xml"))
+
+        self.table_offset = table_offset
+        self.table_full_size = np.array(table_full_size)
+        self.table_half_size = self.table_full_size / 2
+        self.table_friction = table_friction
+
+        self.bin1_body = self.worldbody.find("./body[@name='bin1']")
+        self.table_top_abs = np.array(bin1_pos)
+        self.bin1_pos = bin1_pos
+
+        self.configure_location()
+
+    def configure_location(self):
+        """Configures correct locations for this arena"""
+        self.floor.set("pos", array_to_string(self.bottom_pos))
+        self.bin1_body.set("pos", array_to_string(self.bin1_pos))
 
 class BinsArena(Arena):
     """
